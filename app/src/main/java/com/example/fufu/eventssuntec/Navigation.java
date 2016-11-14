d562d81afb5a4ec582d88e0c53c2e79ec2d1a3a9
fuen809 <fuen.ong.2014@sis.smu.edu.sg>
2016-11-14 13:56:43 +0800

    Using buttons

--- app/src/main/java/com/example/fufu/eventssuntec/Navigation.java
+++ app/src/main/java/com/example/fufu/eventssuntec/Navigation.java
@@ -1,98 +1,72 @@
 package com.example.fufu.eventssuntec;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.support.v4.view.ViewPager;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-import com.example.fufu.sliding.SlidingTabLayout;
-import com.example.fufu.sliding.ViewPagerAdapter;
 
-public class Navigation extends AppCompatActivity {
+public class Navigation extends AppCompatActivity implements View.OnClickListener{
 // Declaring Your View and Variables
 
-    Toolbar toolbar;
-    ViewPager pager;
-    ViewPagerAdapter adapter;
-    SlidingTabLayout tabs;
-    CharSequence Titles[]={"Home","Events"};
-    int Numboftabs =2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100,200); // The desired size of the child
-        params.setMargins(50,50, 50, 50); // Position at 50,50
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100, 200); // The desired size of the child
+        params.setMargins(50, 50, 50, 50); // Position at 50,50
         View v = new ImageView(getBaseContext());
         ImageView image;
         image = new ImageView(v.getContext());
         image.setImageDrawable(v.getResources().getDrawable(R.drawable.pin));
         RelativeLayout mRelativeLayout = (RelativeLayout) findViewById(R.id.nav);
-        mRelativeLayout.addView( image, params);
+        mRelativeLayout.addView(image, params);
 
 
 //        Bitmap b = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
 //        Canvas c = new Canvas(b);
 
-        // Creating The Toolbar and setting it as the Toolbar for the activity
+        //setting onclick listeners
+        Button one = (Button) findViewById(R.id.oneButton);
+        one.setOnClickListener(this); // calling onClick() method
+        Button two = (Button) findViewById(R.id.twoButton);
+        two.setOnClickListener(this);
+        Button three = (Button) findViewById(R.id.threeButton);
+        three.setOnClickListener(this);
 
-        toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolbar);
-
-
-        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
-        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
-
-        // Assigning ViewPager View and setting the adapter
-        pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
-
-        // Assiging the Sliding Tab Layout View
-        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
-        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
-
-        // Setting Custom Color for the Scroll bar indicator of the Tab View
-        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-            @Override
-            public int getIndicatorColor(int position) {
-                return getResources().getColor(R.color.tabsScrollColor);
-            }
-        });
-
-        // Setting the ViewPager For the SlidingTabsLayout
-        tabs.setViewPager(pager);
 
     }
+    @Override
+    public void onClick(View v) {
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+        switch (v.getId()) {
+
+            case R.id.oneButton:
+                // do your code
+                Intent myIntent = new Intent(Navigation.this, Navigation.class);
+                Navigation.this.startActivity(myIntent);
+                break;
+
+            case R.id.twoButton:
+                // do your code
+                Intent myIntent2 = new Intent(Navigation.this, NavigationL2.class);
+                Navigation.this.startActivity(myIntent2);
+                break;
+
+            case R.id.threeButton:
+                // do your code
+                Intent myIntent3 = new Intent(Navigation.this, NavigationL2.class);
+                Navigation.this.startActivity(myIntent3);
+                break;
+
+            default:
+                break;
+        }
+
+    }
 }
---
+++ app/src/main/java/com/example/fufu/eventssuntec/NavigationL2.java
@@ -0,0 +1,13 @@
+package com.example.fufu.eventssuntec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationL2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_l2);
+    }
+}
